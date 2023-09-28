c1f522a95756ef574b3f3635f607671822d465fb
yongjich <yongjichen97@gmail.com>
2023-09-28 19:15:28 -0400

    sync

--- StartSatisafactory.bat
+++ StartSatisafactory.bat
@@ -1,5 +1,6 @@
 @REM start "Satisfactory" "C:\Program Files (x86)\Steam\steamapps\common\Satisfactory\FactoryGame.exe"
 start "child" "C:\Users\Kisuna\AppData\Local\FactoryGame\SatisfactorySynchronizer\runFirstTime.bat"
+timeout 1
 cd "C:\Users\Kisuna\AppData\Local\FactoryGame\SatisfactorySynchronizer\src"
 node "index.js"
 @pause
