d483daf89bbc37b86c23064c63f18978997568b4
unknown <3l.liu444@gmail.com>
2023-10-11 12:02:48 -0400

    new changes

---
+++ StartSatisafactory.bat
@@ -0,0 +1,4 @@
+start "Satisfactory" "D:\SteamLibrary\steamapps\common\Satisfactory\FactoryGame.exe"
+cd "C:\Users\LouDaPC\AppData\Local\FactoryGame\SatisfactorySynchronizer\src"
+node "index.js"
+@pause
