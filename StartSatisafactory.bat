98f4732a57f4922ca7215b9b2eaf9261e0c43192
yongjich <yongjichen97@gmail.com>
2023-10-12 19:25:56 -0400

    remove script

--- StartSatisafactory.bat
+++
@@ -1,4 +0,0 @@
-start "Satisfactory" "C:\Program Files (x86)\Steam\steamapps\common\Satisfactory\FactoryGame.exe"
-cd "C:\Users\Kisuna\AppData\Local\FactoryGame\SatisfactorySynchronizer\src"
-node "index.js"
-@pause
